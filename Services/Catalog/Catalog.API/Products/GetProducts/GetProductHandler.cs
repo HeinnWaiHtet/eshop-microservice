aee03cf0b6b96b0414fae5c8870c22f5be8989a0
ayechanmay <cuheinwaihtet@gmail.com>
2024-07-24 18:27:29 +0900

    adding pagination using Mediator

--- Services/Catalog/Catalog.API/Products/GetProducts/GetProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductHandler.cs
@@ -1,9 +1,8 @@
 ﻿
-using Marten.Linq.QueryHandlers;
 
 namespace Catalog.API.Products.GetProducts;
 
-public record GetProductsQuery(): IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 
 public record GetProductsResult(IEnumerable<Product> Products);
 
@@ -16,9 +15,13 @@ public class GetProductHandler
         this._session = session;
     }
 
-    public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        var products = await this._session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await this._session.Query<Product>()
+            .ToPagedListAsync(
+            query.PageNumber ?? 1,
+            query.PageSize ?? 10,
+            cancellationToken);
 
         return new GetProductsResult(products);
     }
